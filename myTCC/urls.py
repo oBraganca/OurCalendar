79daf566acb5aff3010ae22bae3ed35c73b2f9d4
Thayllon Ryan <thayllonryan17@gmail.com>
2022-11-15 13:54:56 -0300

    b

--- myTCC/urls.py
+++ myTCC/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf.urls.static import static
+from . import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("ourcalendar.urls")),
     path('account/',include('users.urls', namespace="accounts")),
 ]
+urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
